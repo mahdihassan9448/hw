72d4fd5ca600182b3fad9ac784eea9d330b6556e
unknown <mahdihassan9448@gmail.com>
2023-02-19 18:26:55 +0600

    new program

---
+++ class-12/full square.php
@@ -0,0 +1,18 @@
+<?php
+ $start = 1;
+ $end = 100;
+ $step = 1;
+
+ $line = 0;
+
+for ($caunter = $start; $caunter <= $end; $caunter = $step++ ) 
+{
+  echo "The number is: $caunter <br>";
+}
+
+
+// for ($x = 0; $x <= 10; $x++)
+// {
+
+//   echo "The number is: $x <br>";
+// }
