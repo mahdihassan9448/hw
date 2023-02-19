f3657b3dd7399879ddc9ace629365001cb12d761
unknown <mahdihassan9448@gmail.com>
2023-02-19 20:11:39 +0600

    ahasgjaj  #hhe'' will be ignored, and an empty message aborts the commit.

--- class-12/full square.php
+++ class-12/full square.php
@@ -1,18 +1,12 @@
 <?php
- $start = 1;
- $end = 100;
- $step = 1;
+$number = 25;
 
- $line = 0;
+for ($counter = 1; $counter <= $number; $counter++ ) 
+{ 
+    if($counter*$counter == $number)
+    {
+        echo $counter;
+    }
 
-for ($caunter = $start; $caunter <= $end; $caunter = $step++ ) 
-{
-  echo "The number is: $caunter <br>";
+
 }
-
-
-// for ($x = 0; $x <= 10; $x++)
-// {
-
-//   echo "The number is: $x <br>";
-// }
